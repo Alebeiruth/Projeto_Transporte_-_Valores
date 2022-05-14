540622223a65db1d44d4b28385f1206e24553717
Alexandre Beiruth <101944430+Alebeiruth@users.noreply.github.com>
2022-05-14 16:14:26 -0300

    Update transporte_03.py

--- transporte_03.py
+++ transporte_03.py
@@ -5,11 +5,11 @@ custo = ""
 
 ## Transporte Terrestre ##
 
-if peso <= 3:
+if peso <= 300:
   custo = peso * 2.5 + 20
-elif peso <= 5:
+elif peso <= 500:
   custo = peso * 5.0 + 20
-elif peso <= 7:
+elif peso <= 700:
   custo = peso * 6.0 + 20
 else:
   custo = peso * 8.50 + 20
@@ -20,11 +20,11 @@ print("Custo de Transporte Terrestre $", custo)
 
 custo_premium = 125.0
 
-if peso <= 3:
+if peso <= 300:
   custo_premium = peso * 3.5
-elif peso <= 5:
+elif peso <= 500:
   custo_premium = peso * 7.0
-elif peso <= 7:
+elif peso <= 700:
   custo_premium = peso * 10.0
 else:
   custo_premium = peso * 13.10
@@ -33,11 +33,11 @@ print("Custo de Transporte Premium $", custo_premium)
 
 ## Transporte por Drone ##
 
-if peso <= 3:
+if peso <= 300:
   custo_drone = peso * 5.5
-elif peso <= 5:
+elif peso <= 500:
   custo_drone = peso * 11.0
-elif peso <= 7:
+elif peso <= 700:
   custo_drone = peso * 15.50
 else:
   custo_drone = peso * 16.25
